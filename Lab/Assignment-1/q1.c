9438b3c57914cf85e49d606bfd9b7b9a350a4c7a
Muhammad Tariq <muhammad.tariq.19.03.2005@gmail.com>
2026-01-22 00:01:09 +0500

    FOP Lab

--- Lab/Assignment-1/q1.c
+++ Lab/Assignment-1/q1.c
@@ -1,30 +1,24 @@
 #include<stdio.h>
-// Input 15 integers into an array. Count even and odd numbers and store odd numbers in a new array.
 int main (){
-    int arr [15];
-    int even,odd;
-    int newarr[15];
-    even=odd=0;
-    newarr[odd] = '\0';
+    int NumArr [15]; 
+    int even = 0,odd = 0; 
+    int Oddarr[15]; 
     for(int i =0;i<15;i++){
-        printf("Enter the %d number:",i);
-        scanf("%d",&arr[i]);
-        // arr[i] = i+1;
-        if(arr[i]%2!=0){
-            newarr[odd]=arr[i];
-            odd++;
-            newarr[odd]='\0';
+        printf("Enter the value at %d:",i);
+        scanf("%d",&NumArr[i]);
+        if(NumArr[i]%2!=0){ 
+            Oddarr[odd]=NumArr[i];  
+            odd++; 
         }else{
-            even++;
+            even++; 
         }
     }  
-    int i=0;
-    while(newarr[i] != '\0'){
-        printf("%d\t",newarr[i]);
-        i++;
+    printf("Odd Array: ");
+    for(int j=0;j<odd;j++){
+        printf("%d  ",Oddarr[j]); 
     }
     printf("\n");
-    printf("Odd :%d\n",odd);
-    printf("Even :%d\n",even);
+    printf("Odd :%d\n",odd); 
+    printf("Even :%d\n",even); 
     return 0;
 }
